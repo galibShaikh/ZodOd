892769ef389e43c996f6ffffa57e2e2e909b3269
galibShaikh <shaikhgalib2211@gmail.com>
2020-08-25 13:42:33 +0600

    new file added

---
+++ run.java
@@ -0,0 +1,8 @@
+public class Lamborgini {
+  int price;
+   public Lamborgini(int i){
+     int=i }
+     public static void main (String [] args )
+   Lamborgini object=new Lamborgini(70000000);
+ System.out.println("Price of Lambo is:"+object.price);
+}
